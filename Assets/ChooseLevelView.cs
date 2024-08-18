bb1bdb8c3bb708cdcb8321df79438c0732ad9b66
Fantomext <fantomext80@gmail.com>
2024-08-18 16:26:25 +0300

    Add Window

---
+++ Assets/ChooseLevelView.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseLevelView : MonoBehaviour
+{
+    [SerializeField] private Button _levelOne;
+    [SerializeField] private Button _levelTwo;
+    [SerializeField] private Button _levelThree;
+}
---
+++ Assets/VolumeSettings.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSettings : MonoBehaviour
+{
+    [SerializeField] 
+    private Slider mainVolume;
+
+    [SerializeField] 
+    private Slider musicVolume;
+
+    [SerializeField] 
+    private Slider sfxVolume;
+
+}
