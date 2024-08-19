ef5e73d2d55817b872e04c2499463c70cc62d932
Pacelin <Warfacer2020@gmail.com>
2024-08-19 18:59:47 +0300

    sdasd

--- Assets/Plugins/UniOwl/Celestials/Scripts/Planet.cs
+++ Assets/Plugins/UniOwl/Celestials/Scripts/Planet.cs
@@ -20,6 +20,23 @@ namespace UniOwl.Celestials
 
         private void Awake()
         {
+            transform.gameObject.SetActive(false);
+            CombineInstance[] combine = new CombineInstance[_faces.Length];
+            for (int i = 0; i < _faces.Length; i++)
+            {
+                combine[i].mesh = _faces[i].Filter.sharedMesh;
+                combine[i].transform = _faces[i].transform.localToWorldMatrix;
+            }
+
+            Mesh combinedMesh = new Mesh();
+            combinedMesh.CombineMeshes(combine);
+            gameObject.AddComponent<MeshFilter>().sharedMesh = combinedMesh;
+            gameObject.AddComponent<MeshRenderer>();
+            foreach (var face in _faces)
+                face.gameObject.SetActive(false);
+            transform.gameObject.SetActive(true);
+            combinedMesh.bounds = new Bounds(Vector3.zero, Vector3.one * 2000);
+            combinedMesh.RecalculateNormals();
             foreach (var mesh in SharedMeshes)
                 mesh.bounds = new Bounds(Vector3.zero, Vector3.one * 2000);
         }
