d6a593fd3e422ce4a2418d2b65bb9d0db689afd4
Pacelin <Warfacer2020@gmail.com>
2024-08-19 21:14:51 +0300

    feat: New Planet Info

--- Assets/_Project/Runtime/Gameplay/Planets/UI/PlanetInfoView.cs
+++ Assets/_Project/Runtime/Gameplay/Planets/UI/PlanetInfoView.cs
@@ -5,15 +5,15 @@ namespace Runtime.Gameplay.Planets.UI
 {
     public class PlanetInfoView : MonoBehaviour
     {
-        [SerializeField] private Slider _temperatureSlider;
-        [SerializeField] private Slider _oxygenSlider;
-        [SerializeField] private Slider _waterSlider;
+        [SerializeField] private Image _temperatureSlider;
+        [SerializeField] private Image _oxygenSlider;
+        [SerializeField] private Image _waterSlider;
 
         public void SetTemperature(float percent) =>
-            _temperatureSlider.value = percent;
+            _temperatureSlider.fillAmount = percent;
         public void SetOxygen(float percent) =>
-            _oxygenSlider.value = percent;
+            _oxygenSlider.fillAmount = percent;
         public void SetWater(float percent) =>
-            _waterSlider.value = percent;
+            _waterSlider.fillAmount = percent;
     }
 }
