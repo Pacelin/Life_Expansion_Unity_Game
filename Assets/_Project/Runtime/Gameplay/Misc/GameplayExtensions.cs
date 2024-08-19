df8b088f9918fa4dd755a4c4ffb1d0f964781274
Pacelin <Warfacer2020@gmail.com>
2024-08-19 18:36:55 +0300

    feat: ADd digit for 1K

--- Assets/_Project/Runtime/Gameplay/Misc/GameplayExtensions.cs
+++ Assets/_Project/Runtime/Gameplay/Misc/GameplayExtensions.cs
@@ -10,13 +10,13 @@
             {
                 var mod = value % 1000;
                 if (mod >= 100)
-                    return value / 1000 + "." + mod / 100 + "K";
+                    return value / 1000 + "." + mod / 10 + "K";
                 return value / 1000 + "K";
             }
 
             var modM = value % 1000000;
             if (modM >= 100000)
-                return value / 1000000 + "." + modM / 100000 + "M";
+                return value / 1000000 + "." + modM / 10000 + "M";
             return value / 1000000 + "M";
         }
     }
