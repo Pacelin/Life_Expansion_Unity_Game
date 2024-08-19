f8cf1590e41e615162cb47c0ec0d81ef08161705
Pacelin <Warfacer2020@gmail.com>
2024-08-20 01:41:13 +0300

    feat: Unlocking Extension

--- Assets/_Project/Runtime/Gameplay/Buildings/Conditions/BuildingUnlockCondition.cs
+++ Assets/_Project/Runtime/Gameplay/Buildings/Conditions/BuildingUnlockCondition.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Localization;
 using Zenject;
 
 namespace Runtime.Gameplay.Buildings.Conditions
 {
     public abstract class BuildingUnlockCondition : ScriptableObject
     {
+        public string LockingDescription => _lockingDescription.GetLocalizedString();
+        
+        [SerializeField] private LocalizedString _lockingDescription;
+        
         public abstract IDisposable Subscribe(DiContainer container, Action<bool> observer);
     }
 }
