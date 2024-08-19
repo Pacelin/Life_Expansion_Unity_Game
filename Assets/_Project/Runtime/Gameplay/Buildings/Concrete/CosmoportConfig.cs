e51a849ede0c769ccd721409c09a23d1dd9c2b6b
Pacelin <Warfacer2020@gmail.com>
2024-08-19 19:38:11 +0300

    feat: Cosmoport

---
+++ Assets/_Project/Runtime/Gameplay/Buildings/Concrete/CosmoportConfig.cs
@@ -0,0 +1,16 @@
+﻿using Runtime.Gameplay.Buildings.General;
+using UnityEngine;
+using Zenject;
+
+namespace Runtime.Gameplay.Buildings.Concrete
+{
+    [CreateAssetMenu(menuName = "Gameplay/Buildings/Cosmoport", fileName = "Cosmoport")]
+    public class CosmoportConfig : BuildingConditionalConfig
+    {
+        public override BuildingParameterEntry[] GetAdditionalParameters() =>
+            new BuildingParameterEntry[] { };
+
+        public override IBuildingModel CreateModel(DiContainer container, BuildingView view) =>
+            container.Instantiate<CosmoportModel>(new object[] { this, view });
+    }
+}
