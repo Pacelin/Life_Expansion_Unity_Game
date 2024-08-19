420ecb5307318e104a750927d0d38ffb4ea55156
Pacelin <Warfacer2020@gmail.com>
2024-08-19 19:36:49 +0300

    feat: Living House

---
+++ Assets/_Project/Runtime/Gameplay/Buildings/Concrete/LivingHouseConfig.cs
@@ -0,0 +1,20 @@
+﻿using Runtime.Gameplay.Buildings.General;
+using UnityEngine;
+using Zenject;
+
+namespace Runtime.Gameplay.Buildings.Concrete
+{
+    [CreateAssetMenu(menuName = "Gameplay/Buildings/Living House", fileName = "Living House")]
+    public class LivingHouseConfig : BuildingConditionalConfig
+    {
+        public int ColonizersMax => (int) _colonizersMaxDelta.Value;
+        
+        [SerializeField] private BuildingParameterEntry _colonizersMaxDelta;
+
+        public override BuildingParameterEntry[] GetAdditionalParameters() =>
+            new[] { _colonizersMaxDelta };
+
+        public override IBuildingModel CreateModel(DiContainer container, BuildingView view) =>
+            container.Instantiate<LivingHouseModel>(new object[] { this, view });
+    }
+}
---
+++ Assets/_Project/Runtime/Gameplay/Buildings/Concrete/LivingHouseModel.cs
@@ -0,0 +1,21 @@
+﻿using Runtime.Gameplay.Buildings.General;
+
+namespace Runtime.Gameplay.Buildings.Concrete
+{
+    public class LivingHouseModel : BuildingModel<LivingHouseConfig>
+    {
+        protected override void OnEnable()
+        {
+            _colonizers.Population.ApplyDeltaToMax(_config.ColonizersMax);
+        }
+
+        protected override void OnDisable()
+        {
+            _colonizers.Population.ApplyDeltaToMax(-_config.ColonizersMax);
+        }
+
+        protected override void Dispose()
+        {
+        }
+    }
+}
