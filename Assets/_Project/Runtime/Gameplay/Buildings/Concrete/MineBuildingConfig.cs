91025761f6b66c6a2abfb998c4bdb7f70fee20d2
Pacelin <Warfacer2020@gmail.com>
2024-08-19 19:34:15 +0300

    feat: Mine

---
+++ Assets/_Project/Runtime/Gameplay/Buildings/Concrete/MineBuildingConfig.cs
@@ -0,0 +1,22 @@
+﻿using Runtime.Gameplay.Buildings.General;
+using UnityEngine;
+using Zenject;
+
+namespace Runtime.Gameplay.Buildings.Concrete
+{
+    [CreateAssetMenu(menuName = "Gameplay/Buildings/Mine Building", fileName = "Mine Building")]
+    public class MineBuildingConfig : BuildingConditionalConfig
+    {
+        public int Production => (int) _production.Value;
+        public float OxygenDelta => _oxygenDelta.Value;
+        
+        [SerializeField] private BuildingParameterEntry _oxygenDelta;
+        [SerializeField] private BuildingParameterEntry _production;
+
+        public override BuildingParameterEntry[] GetAdditionalParameters() =>
+            new[] { _production, _oxygenDelta };
+
+        public override IBuildingModel CreateModel(DiContainer container, BuildingView view) =>
+            container.Instantiate<MineBuildingModel>(new object[] { this, view });
+    }
+}
---
+++ Assets/_Project/Runtime/Gameplay/Buildings/Concrete/MineBuildingModel.cs
@@ -0,0 +1,21 @@
+﻿using Runtime.Gameplay.Buildings.General;
+
+namespace Runtime.Gameplay.Buildings.Concrete
+{
+    public class MineBuildingModel : BuildingModel<MineBuildingConfig>
+    {
+        protected override void OnEnable()
+        {
+            _planet.Oxygen.ApplyDelta(_config.OxygenDelta);
+        }
+
+        protected override void OnDisable()
+        {
+            _planet.Oxygen.ApplyDelta(-_config.OxygenDelta);
+        }
+
+        protected override void Dispose()
+        {
+        }
+    }
+}
