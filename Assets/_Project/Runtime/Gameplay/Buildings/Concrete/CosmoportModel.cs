9193009f543f283bbc033950b46fd40d9b0d4b68
Pacelin <Warfacer2020@gmail.com>
2024-08-20 19:58:44 +0300

    feat: Cosmoport

--- Assets/_Project/Runtime/Gameplay/Buildings/Concrete/CosmoportModel.cs
+++ Assets/_Project/Runtime/Gameplay/Buildings/Concrete/CosmoportModel.cs
@@ -1,11 +1,16 @@
-﻿using Runtime.Gameplay.Buildings.General;
+﻿using Cysharp.Threading.Tasks;
+using Jamcelin.Runtime.SceneManagement;
+using Runtime.Gameplay.Buildings.General;
+using Zenject;
 
 namespace Runtime.Gameplay.Buildings.Concrete
 {
     public class CosmoportModel : BuildingModel<CosmoportConfig>
     {
+        [Inject] private SceneManager _manager;
         protected override void OnEnable()
         {
+            _manager.SwitchScene(EScene.MainMenu, _ => {}).Forget();
         }
 
         protected override void OnDisable()
